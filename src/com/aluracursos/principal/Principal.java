e6ae013722b4fa61f3bc080ac28f973210a9b56b
PaoBas <paobas113@gmail.com>
2025-05-07 12:55:49 -0300

    Instalando Gson y probando que funcione el Json

--- src/com/aluracursos/principal/Principal.java
+++ src/com/aluracursos/principal/Principal.java
@@ -1,9 +1,26 @@
 package com.aluracursos.principal;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 public class Principal {
     public static void main(String[] args) {
 
-        //atributos
+        // Crear un objeto JSON simulando datos de conversión de moneda
+        JsonObject moneda = new JsonObject();
+        moneda.addProperty("monedaOrigen", "USD");
+        moneda.addProperty("monedaDestino", "ARS");
+        moneda.addProperty("valor", 1050.75);
 
+        // Crear instancia de Gson
+        Gson gson = new Gson();
+
+        // Serializar el objeto a JSON (convertir a String)
+        String json = gson.toJson(moneda);
+        System.out.println("JSON generado: " + json);
+
+        // Deserializar (convertir de String a objeto JSON)
+        JsonObject deserializado = gson.fromJson(json, JsonObject.class);
+        System.out.println("Moneda origen deserializada: " + deserializado.get("monedaOrigen").getAsString());
     }
 }
+
