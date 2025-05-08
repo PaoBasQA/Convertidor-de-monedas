50e37c9d3e824d173180afde8ab3ab336a192d87
PaoBas <paobas113@gmail.com>
2025-05-08 12:22:50 -0300

    Creando mensaje de bienvenida

--- src/com/aluracursos/principal/Menu.java
+++ src/com/aluracursos/principal/Menu.java
@@ -1,5 +1,22 @@
 package com.aluracursos.principal;
 
+import java.util.Scanner;
+
 public class Menu {
 
+    //private Scanner teclado = new Scanner(System.in);
+
+    //public void showMenu() {
+      //  int option;
+        //do {
+          //  System.out.println("\nSeleccione una opción:");
+            //System.out.println("1. Dólar a Pesos Argentinos");
+           // System.out.println("2. Pesos Argentinos a Dólar");
+           // System.out.println("3. Dólar a Reales Brasileros");
+           // System.out.println("4. Reales Brasileros a Dólar");
+           // System.out.println("5. Dólar a Pesos Colombianos");
+           // System.out.println("6. Pesos Colombianos a Dólar");
+           // System.out.println("7. Salir");
+       // }
+   // }
 }
